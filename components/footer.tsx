a9352b658593be9d64dffa969c614f761534ad95
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-27 18:33:22 +0530

    feat: Footer Component

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-white border-t'>
+        <div className='mx-auto py-10'>
+            <p className='text-sm text-black text-center'>
+                &copy; 2023 Store, Inc. All rights reserved
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
