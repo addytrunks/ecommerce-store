7f7d53ec1f40a428f75eaf77d96f546db413e071
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-28 12:14:47 +0530

    feat:  tailwind classes merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,8 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const formatter = new Intl.NumberFormat("en-US",{
-  style:"currency",
-  currency:'USD'
-})
