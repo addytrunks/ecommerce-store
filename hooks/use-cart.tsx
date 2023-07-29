fd23bd2e7a84cd83ccdb0117027bb535dd48d098
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-29 19:41:28 +0530

    add: state management carts

---
+++ hooks/use-cart.tsx
@@ -0,0 +1,40 @@
+import { Product } from "@/types";
+import { toast } from "react-hot-toast";
+import { create } from "zustand";
+import {persist,createJSONStorage} from 'zustand/middleware'
+
+interface CartStore{
+   items:Product[],
+   addItem:(data:Product) => void,
+   removeItem?:(id:string) => void,
+   removeAll?:() => void,
+}
+
+const useCart = create(
+    persist<CartStore>((set,get) => ({
+        items:[],
+        addItem:(data:Product) => {
+            const currentItems = get().items;
+            const existingItems = currentItems.find((item) => item.id === data.id)
+
+            if(existingItems){
+                return toast("Product already in cart")
+            }
+            set({items:[...currentItems,data]})
+            toast.success("Product added to cart")
+        },
+        removeItem:(id:string) => {
+            set({items:[...get().items.filter((item) => item.id !== id)]})
+            toast.success("Product removed from cart")
+        },
+        removeAll:() => {
+            set({items:[]})
+            toast.success("All Products removed from cart.")
+        }
+    }),{
+        name:'cartStorage',
+        storage:createJSONStorage(() => localStorage)
+    })
+)
+
+export default useCart
