6fc1bab2d4598d61a394fbff151e74e3a90e259c
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-29 14:31:59 +0530

    add: fetch functions for category,color,size

---
+++ actions/get-colors.tsx
@@ -0,0 +1,11 @@
+import { Color } from "@/types"
+
+const url = `${process.env.NEXT_PUBLIC_API_URL}/colors`
+
+const getColors = async():Promise<Color[]> => {
+    const res = await fetch(url)
+
+    return res.json()
+}
+
+export default getColors;
